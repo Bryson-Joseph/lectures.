9ec3552fd6dd7814e5ff48880522f52e958c6712
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-03 13:13:42 +0300

    functions,variables,booleans,objects,arrays,const & let

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,23 @@
-alert("hi");
+const amIFat = true
+let something
+console.log(something, amIFat)
+
+const mon = 'mon'
+const tue = 'tue'
+const wed = 'wed'
+const thu = 'thu'
+const fri = 'fri'
+const sat = 'sat'
+const sun = 'sun'
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun]
+const player = {
+    name: 'jeff',
+    points: 10,
+    fat: true,
+}
+console.log(player)
+player.Lastname = 'potato'
+player.points = 15
+console.log(player)
+    //
