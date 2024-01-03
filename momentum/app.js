b8db67df23a5afac70994f7607eea7c4b6ea2827
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-03 17:16:33 +0300

    returns

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,34 @@
-function sayHello() {}
-const player = {
-    name: "Jeff",
-    sayHello: function(otherPersonsName, age) {
-        console.log("hello! " + otherPersonsName + " nice to meet you");
+const calculator = {
+    plus: function(a, b) {
+        return (a + b);
+    },
+    minus: function(a, b) {
+        return (a - b);
+    },
+    times: function(a, b) {
+        return (a * b);
+    },
+    divide: function(a, b) {
+        return (a / b);
+    },
+    power: function(a, b) {
+        return (a ** b);
     },
 };
 
-player.sayHello("Riele");
-player.sayHello("Piper");
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+const age = 96;
+
+function calculateEngAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const EngAge = calculateEngAge(age);
+
+console.log(EngAge);
 //
