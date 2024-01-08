09b603a6df51cc9e50380f905a97175b5b865d67
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-08 18:59:36 +0300

    search-for-elements and Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,34 +1,7 @@
-const calculator = {
-    plus: function(a, b) {
-        return (a + b);
-    },
-    minus: function(a, b) {
-        return (a - b);
-    },
-    times: function(a, b) {
-        return (a * b);
-    },
-    divide: function(a, b) {
-        return (a / b);
-    },
-    power: function(a, b) {
-        return (a ** b);
-    },
-};
+const title = document.querySelector('div.hello:first-child h1');
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+function handleTitle
 
-const age = 96;
-
-function calculateEngAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
-}
-
-const EngAge = calculateEngAge(age);
-
-console.log(EngAge);
-//
+title.addEventListener("click")
+console.log(title)
+    //
