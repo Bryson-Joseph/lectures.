0e71f1d7697bf52d65cf5ca7511370277ab410f7
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-22 16:55:31 +0300

    Mouse-painting

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,25 +3,25 @@ const ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 800
 ctx.lineWidth = 2
+let isPainting = false
 
-const colors = [
-  '#DFFF00',
-  '#FFBF00',
-  '#FF7F50',
-  '#DE3163',
-  '#9FE2BF',
-  '#40E0D0',
-  '#6495ED',
-  '#CCCCFF',
-]
-
-function onClick(event) {
-  ctx.beginPath()
-  ctx.moveTo(900, 900)
-  const color = colors[Math.floor(Math.random() * colors.length)]
-  ctx.lineTo(event.offsetX, event.offsetY)
-  ctx.strokeStyle = color
-  ctx.stroke()
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY)
+    ctx.stroke()
+    return
+  }
+  ctx.moveTo(event.offsetX, event.offsetY)
 }
-canvas.addEventListener('mousemove', onClick)
+
+function onMouseDown() {
+  isPainting = true
+}
+
+function onMouseup() {
+  isPainting = false
+}
+canvas.addEventListener('mousemove', onMove)
+canvas.addEventListener('mousedown', onMouseDown)
+canvas.addEventListener('mouseup', onMouseUp)
 //
