12530f25dcddf88b353c4868403daeef0a22cfaf
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-25 17:02:49 +0300

    Eraser-and-destroy

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,12 +1,16 @@
+const eraserBtn = document.getElementById('eraser-btn')
+const destroyBtn = document.getElementById('destroy-btn')
 const modeBtn = document.getElementById('mode-btn')
 const colorOptions = Array.from(document.getElementsByClassName('color-option'))
-
 const color = document.getElementById('color')
 const lineWidth = document.getElementById('line-width')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-canvas.width = 800
-canvas.height = 800
+const CANVAS_WIDTH = 800
+const CANVAS_HEIGHT = 800
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
 ctx.lineWidth = lineWidth.value
 let isPainting = false
 let isFilling = false
@@ -34,14 +38,14 @@ function onLineWidthChange(event) {
 }
 
 function onColorChange(event) {
-  ctx.StrokeStyle = event.target.value
+  ctx.strokeStyle = event.target.value
   ctx.fillStyle = event.target.value
 }
 
 function onColorClick(event) {
   const colorValue = event.target.dataset.color
   ctx.strokeStyle = colorValue
-  ctxfillStyle = colorValue
+  ctx.fillStyle = colorValue
   color.value = colorValue
 }
 
@@ -57,20 +61,30 @@ function onModeClick() {
 
 function onCanvasClick() {
   if (isFilling) {
-    ctx.fillRect(0, 0, 800, 800)
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   }
 }
 
+function onDestroyClick() {
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = 'white'
+  isFilling = false
+  modeBtn.innerText = 'Fill'
+}
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
 canvas.addEventListener('click', onCanvasClick)
-
 lineWidth.addEventListener('change', onLineWidthChange)
 color.addEventListener('change', onColorChange)
 
 colorOptions.forEach((color) => color.addEventListener('click', onColorClick))
-//
+
 modeBtn.addEventListener('click', onModeClick)
-//
+destroyBtn.addEventListener('click', onDestroyClick)
+eraserBtn.addEventListener('click', onEraserClick)
