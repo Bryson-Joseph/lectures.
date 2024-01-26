cdac067c73eb96589537617ce1278bcd9ed711a9
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-01-26 17:38:13 +0300

    css-style

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,3 +1,6 @@
+const saveBtn = document.getElementById('save')
+const textInput = document.getElementById('text')
+const fileInput = document.getElementById('file')
 const eraserBtn = document.getElementById('eraser-btn')
 const destroyBtn = document.getElementById('destroy-btn')
 const modeBtn = document.getElementById('mode-btn')
@@ -8,10 +11,11 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = 800
 const CANVAS_HEIGHT = 800
-
+//
 canvas.width = CANVAS_WIDTH
 canvas.height = CANVAS_HEIGHT
 ctx.lineWidth = lineWidth.value
+ctx.lineCap = 'round'
 let isPainting = false
 let isFilling = false
 
@@ -75,6 +79,36 @@ function onEraserClick() {
   isFilling = false
   modeBtn.innerText = 'Fill'
 }
+
+function onFileChange(event) {
+  const file = event.target.files[0]
+  const url = URL.createObjectURL(file)
+  const image = new Image()
+  image.src = url
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    fileInput.value = null
+  }
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value
+  if (text !== '') {
+    ctx.save()
+    ctx.lineWidth = 1
+    ;(ctx.font = '68px sans-serif'),
+      ctx.fillText(text, event.offsetX, event.offsetY)
+    ctx.restore()
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL()
+  const a = document.createElement('a')
+  a.href = url
+  a.download = 'myDrawing.png'
+  a.click()
+}
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
@@ -88,3 +122,5 @@ colorOptions.forEach((color) => color.addEventListener('click', onColorClick))
 modeBtn.addEventListener('click', onModeClick)
 destroyBtn.addEventListener('click', onDestroyClick)
 eraserBtn.addEventListener('click', onEraserClick)
+fileInput.addEventListener('change', onFileChange)
+//
