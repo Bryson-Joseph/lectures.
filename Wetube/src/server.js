b15d7e42add91182f5aa508e69c4b08cacf64ae7
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-01 18:58:46 +0300

    install-morgan

--- Wetube/src/server.js
+++ Wetube/src/server.js
@@ -1,26 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const PORT = 40000
 
 const app = express()
+const logger = morgan('dev')
 
-const routerLogger = (req, res, next) => {
-  console.log('PATH', req.path)
-  next()
-}
-
-const methodLogger = (req, res, next) => {
-  console.log('METHOD', req.method)
-  next()
-}
 const home = (req, res) => {
-  console.log('I will respond.')
   return res.send('hello')
 }
 const login = (req, res) => {
   return res.send('login')
 }
-app.use(methodLogger, routerLogger)
+app.use(morgan('dev'))
 app.get('/', home)
 app.get('/login', login)
 
