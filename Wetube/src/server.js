f6d208ae953381e6e0b2459f9ba4486755dca44e
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-01 16:34:10 +0300

    middleware-partone-and-two

--- Wetube/src/server.js
+++ Wetube/src/server.js
@@ -4,14 +4,29 @@ const PORT = 40000
 
 const app = express()
 
-const home = (req, res) => res.send('hello')
+const routerLogger = (req, res, next) => {
+  console.log('PATH', req.path)
+  next()
+}
+
+const methodLogger = (req, res, next) => {
+  console.log('METHOD', req.method)
+  next()
+}
+const home = (req, res) => {
+  console.log('I will respond.')
+  return res.send('hello')
+}
+const login = (req, res) => {
+  return res.send('login')
+}
+app.use(methodLogger, routerLogger)
 app.get('/', home)
+app.get('/login', login)
 
 const handleListening = () =>
   console.log(`Server listening on port https://localhost:${PORT}  `)
 
 app.listen(PORT, handleListening)
-
-//
 
 //
