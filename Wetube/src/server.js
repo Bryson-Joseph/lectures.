13f7fdfea5c5f4413054ab8f847a5320241bee02
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-01 12:07:42 +0300

    middleware

--- Wetube/src/server.js
+++ Wetube/src/server.js
@@ -4,15 +4,31 @@ const PORT = 40000
 
 const app = express()
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`)
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+}
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url
+  if (url === '/protected') {
+    return res.send('<h1>Not Allowed</h1>')
+  }
+  console.log('Allowed, you may continue ')
   next()
 }
 
 const handleHome = (req, res) => {
-  return res.send('I love middlewares')
+  return res.send('I love Middlewares')
 }
-app.get('/', gossipMiddleware, handleHome)
+
+const handleProtected = (req, res) => {
+  return res.send('Welcome to the private lounge.')
+}
+
+app.use(logger, privateMiddleware)
+app.get('/', handleHome)
+app.get('/protected', handleProtected)
 
 const handleListening = () =>
   console.log(`Server listening on port https://localhost:${PORT}  `)
