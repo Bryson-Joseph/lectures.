f1aa772ef39fa5b4604b91b1fd55d1512b6c301e
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-02 20:05:20 +0300

    cleaning-the-code

--- Wetube/src/server.js
+++ Wetube/src/server.js
@@ -1,20 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import globalRouter from './routers/globalRouter'
 const PORT = 40000
 
 const app = express()
 const logger = morgan('dev')
 
-const home = (req, res) => {
-  return res.send('hello')
-}
-const login = (req, res) => {
-  return res.send('login')
-}
-app.use(morgan('dev'))
-app.get('/', home)
-app.get('/login', login)
+app.use(logger)
+app.use('/videos', videoRouter)
+app.use('/users', userRouter)
+app.use('/', globalRouter)
 
 const handleListening = () =>
   console.log(`Server listening on port https://localhost:${PORT}  `)
