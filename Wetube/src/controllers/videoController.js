3c01200a0d14fff2580fcf93ec287a3203bd11ef
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-07 19:56:09 +0300

    config-pug-and-partials

--- Wetube/src/controllers/videoController.js
+++ Wetube/src/controllers/videoController.js
@@ -1,11 +1,7 @@
 export const trending = (req, res) => res.render('home')
-export const see = (req, res) => {
-  return res.send(`<DOCTYPE html><html lang="ko"><head><title>Wetube</title></head><body><h1>Watch Video #${req.params.id}</h1></body><footer>&copy;2024 - All right are reserved</footer></html>
-  `)
-}
-
+export const see = (req, res) => res.render('watch')
 export const edit = (req, res) => {
-  return res.send(
+  return res.render(
     `<DOCTYPE html><html lang="ko"><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1></body><footer>&copy;2024 - All right are reserved</footer></html>`
   )
 }
