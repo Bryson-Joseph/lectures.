7e80d62561995eee1f74b19956d3cbd17a149bb1
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-07 08:29:14 +0300

    config-pug

--- Wetube/src/controllers/videoController.js
+++ Wetube/src/controllers/videoController.js
@@ -1,10 +1,13 @@
-export const trending = (req, res) => res.send('Home Page Videos')
+export const trending = (req, res) => res.render('home')
 export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`)
+  return res.send(`<DOCTYPE html><html lang="ko"><head><title>Wetube</title></head><body><h1>Watch Video #${req.params.id}</h1></body><footer>&copy;2024 - All right are reserved</footer></html>
+  `)
 }
 
 export const edit = (req, res) => {
-  return res.send('Edit')
+  return res.send(
+    `<DOCTYPE html><html lang="ko"><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1></body><footer>&copy;2024 - All right are reserved</footer></html>`
+  )
 }
 export const search = (req, res) => res.send('Search')
 export const upload = (req, res) => res.send('Upload')
