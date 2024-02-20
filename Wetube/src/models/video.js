89cf54d3c49f80525e0a6e99c7eb77c97f9319dd
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-20 19:02:36 +0300

    Statics

--- Wetube/src/models/video.js
+++ Wetube/src/models/video.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose'
 
+export const formatHashtags = (hashtags) =>
+  hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`))
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   description: { type: String, required: true, trim: true, minLength: 20 },
@@ -11,10 +14,10 @@ const videoSchema = new mongoose.Schema({
   },
 })
 
-videoSchema.pre('save', async function () {
-  this.hashtags = this.hashtags[0]
+videoSchema.static('formatHashtags', function (hashtags) {
+  return hashtags
     .split(',')
-    .localeCompare((word) => (word.startsWith('#') ? word : `#${word}`))
+    .map((word) => (word.startsWith('#') ? word : `#${word}`))
 })
 
 const video = mongoose.model('Video', videoSchema)
