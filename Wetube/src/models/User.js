27ffc5bf7823944e54f44d16aaf103e6f14c4bf1
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-26 19:59:28 +0300

    login-part-two

--- Wetube/src/models/User.js
+++ Wetube/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
@@ -12,5 +13,7 @@ userSchema.pre('save', async function () {
   this.password = await bcrypt.hash(this.password, 5)
   console.log('Hashed password', this.password)
 })
+
 const User = mongoose.model('User', userSchema)
+
 export default User
