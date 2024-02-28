9aa8e33d10b05f65606c2c87d2cac66cf5a3779b
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-02-28 09:35:25 +0300

    session-and-cookies-part-two

--- Wetube/src/controllers/userController.js
+++ Wetube/src/controllers/userController.js
@@ -2,64 +2,64 @@ import User from '../models/User'
 import bcrypt from 'bcrypt'
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' })
-export const postJoin = async (req, res) => {
-  const { name, username, email, password, password2, location } = req.body
-  const pageTitle = 'Join'
-  if (password !== password2) {
-    return res.status(400).render('join', {
-      pageTitle,
-      errorMessage: 'Password confirmation does not match.',
+export const postJoin = async(req, res) => {
+    const { name, username, email, password, password2, location } = req.body
+    const pageTitle = 'Join'
+    if (password !== password2) {
+        return res.status(400).render('join', {
+            pageTitle,
+            errorMessage: 'Password confirmation does not match.',
+        })
+    }
+    const exists = await User.exists({
+        $or: [{ username }, { email }],
     })
-  }
-  const exists = await User.exists({
-    $or: [{ username }, { email }],
-  })
-  if (exists) {
-    return res.status(400).render('join', {
-      pageTitle,
-      errorMessage: 'This username/email is already taken.. ',
-    })
-  }
-  try {
-    await User.create({
-      name,
-      username,
-      email,
-      password,
-      location,
-    })
-    return res.redirect('/login')
-  } catch (error) {
-    return res.status(400).render('join', {
-      pageTitle: 'Upload Video',
-      errorMessage: error._message,
-    })
-  }
+    if (exists) {
+        return res.status(400).render('join', {
+            pageTitle,
+            errorMessage: 'This username/email is already taken.. ',
+        })
+    }
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        })
+        return res.redirect('/login')
+    } catch (error) {
+        return res.status(400).render('join', {
+            pageTitle: 'Upload Video',
+            errorMessage: error._message,
+        })
+    }
 }
 
 export const getLogin = (req, res) =>
-  res.render('login', { pageTitle: 'Login' })
+    res.render('login', { pageTitle: 'Login' })
 
-export const postLogin = async (req, res) => {
-  const { username, password } = req.body
-  const exists = await User.exists({ username })
-  if (!exists) {
-    return res.status(400).render('login', {
-      pageTitle: 'Login',
-      errorMessage: 'An account with this username does not exists.',
-    })
-  }
-  const ok = await bcrypt.compare(password, user.password)
-  if (!ok) {
-    return res.status(400).render('login', {
-      pageTitle,
-      errorMessage: 'Wrong password',
-    })
-  }
-  return res.redirect('/')
-}
+export const postLogin = async(req, res) => {
+        const { username, password } = req.body
+        const exists = await User.exists({ username })
+        if (!exists) {
+            return res.status(400).render('login', {
+                pageTitle: 'Login',
+                errorMessage: 'An account with this username does not exists.',
+            })
+        }
+        const ok = await bcrypt.compare(password, user.password)
+        if (!ok) {
+            return res.status(400).render('login', {
+                pageTitle,
+                errorMessage: 'Wrong password',
+            })
+        }
+        return res.redirect('/')
 
-export const edit = (req, res) => res.send('Edit User')
-export const remove = (req, res) => res.send('Remove User')
-export const logout = (req, res) => res.send('log out')
-export const see = (req, res) => res.send('See User')
+
+        export const edit = (req, res) => res.send('Edit User')
+        export const remove = (req, res) => res.send('Remove User')
+        export const logout = (req, res) => res.send('log out')
+        export const see = (req, res) => res.send('See User')
