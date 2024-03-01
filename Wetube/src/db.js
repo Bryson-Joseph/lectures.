76f77f5f453f8172cafb126263713017e34347fd
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-01 18:36:37 +0300

    user-bcrypt

--- Wetube/src/db.js
+++ Wetube/src/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     })
     console.log('Connected to MongoDB')
   } catch (error) {
-    console.error('MongoDB conneeection error:', error)
+    console.error('MongoDB connection error:', error)
     process.exit(1)
   }
 }
