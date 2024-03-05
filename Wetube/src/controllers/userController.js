b63f6c27a2a8d4e165593b65ebad899c26016d04
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-05 12:00:56 +0300

    github-part-three

--- Wetube/src/controllers/userController.js
+++ Wetube/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { application } from 'express'
 import User from '../models/User'
 import bcrypt from 'bcrypt'
 
@@ -64,7 +65,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = `https://github.com/login/oauth/authorize`
   const config = {
-    client_id: '26ce27c4b2d325366497',
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: 'read:user user:email',
   }
@@ -73,7 +74,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl)
 }
 
-export const finishGithubLogin = (req, res) => {}
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = 'https://github.com/login/oauth/acsess_token'
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  }
+  const params = new URLSearchParams(config).toString()
+  const finalUrl = `${baseUrl}?${params}`
+  const data = await fetch(finalUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+  const json = await data.json()
+  console.log(json)
+}
 
 export const edit = (req, res) => res.send('Edit User')
 export const remove = (req, res) => res.send('Remove User')
