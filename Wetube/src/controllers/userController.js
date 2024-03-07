9331e5c32f6980c8b770f61bb7d4af866d180e56
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-07 16:20:48 +0300

    edit-profile-part-one-and-two

--- Wetube/src/controllers/userController.js
+++ Wetube/src/controllers/userController.js
@@ -144,7 +144,26 @@ export const logout = (req, res) => {
 export const getEdit = (req, res) => {
   return res.render('edit-profile', { pageTitle: 'Edit Profile' })
 }
-export const postEdit = (req, res) => {
-  return res.render('edit-profile')
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req
+
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  )
+  req.session.user = findByIdAndUpdate
+  return res.redirect('/users/edit')
 }
+
 export const see = (req, res) => res.send('See User')
