58d44bd2f58f6e9af876c124f6e962d6dc0d2c7e
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-08 12:49:48 +0300

    file-uploads-part-one-and-two

--- Wetube/src/routers/userRouter.js
+++ Wetube/src/routers/userRouter.js
@@ -9,19 +9,29 @@ import {
   getChangePassword,
   postChangePassword,
 } from '../controllers/userController'
-import { protectorMiddleware, publicOnlyMiddleware } from '../middlewares'
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware,
+  uploadFiles,
+} from '../middlewares'
 
 const userRouter = express.Router()
 
 userRouter.get('/logout', protectorMiddleware, logout)
-userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin)
-userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin)
-userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(postEdit)
+userRouter
+  .route('/edit')
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(uploadFiles.single('avatar'), postEdit)
 userRouter
   .route('/change-password')
   .all(protectorMiddleware)
   .get(getChangePassword)
   .post(postChangePassword)
+
+userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin)
+userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin)
+
 userRouter.get(':id', see)
 
 export default userRouter
