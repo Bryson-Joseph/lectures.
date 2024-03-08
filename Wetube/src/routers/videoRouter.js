66ed81e91d898085f10cfa10837871308940f7b0
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-08 16:36:08 +0300

    video-upload

--- Wetube/src/routers/videoRouter.js
+++ Wetube/src/routers/videoRouter.js
@@ -7,24 +7,26 @@ import {
   postUpload,
   deleteVideo,
 } from '../controllers/videoController'
-import { protectorMiddleware } from '../middlewares'
+import { videoUpload, protectorMiddleware } from '../middlewares'
 
 const videoRouter = express.Router()
-
-videoRouter
-  .route('/upload')
-  .all(protectorMiddleware)
-  .get(getUpload)
-  .post(postUpload)
-videoRouter.get('/:id([0-9a-f]{24})', watch)
 videoRouter
   .route('/:id([0-9a-f]{24})/edit')
   .all(protectorMiddleware)
   .get(getEdit)
   .post(postEdit)
+
 videoRouter
   .route('/:id([0-9a-f]{24})/delete')
   .all(protectorMiddleware)
   .get(deleteVideo)
+
+videoRouter
+  .route('/upload')
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(videoUpload.single('video'), postUpload)
+
+videoRouter.get('/:id([0-9a-f]{24})', watch)
 
 export default videoRouter
