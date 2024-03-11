15e1e22089e800dfedb79c9848a5a99f697bceda
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-11 19:03:02 +0300

    users-videos

--- Wetube/src/controllers/videoController.js
+++ Wetube/src/controllers/videoController.js
@@ -1,4 +1,4 @@
-import Video from '../models/Video'
+import Video from '../models/video'
 import user from '../models/User'
 
 export const home = async (req, res) => {
@@ -8,8 +8,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params
-  const video = await Video.findById(id)
-  console.log(video)
+  const video = await video.findById(id).populate('owner')
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' })
   }
@@ -48,12 +47,16 @@ export const postUpload = async (req, res) => {
   const { path: fileUrl } = req.file
   const { title, description, hashtags } = req.body
   try {
-    await Video.create({
+    const newVideo = await Video.create({
       title,
       description,
       fileUrl,
+
       hashtags: Video.formatHashtags(hashtags),
     })
+    const user = await User.findById(_id)
+    user.videos.push(newVideo._id)
+    user.save()
     return res.redirect('/')
   } catch (error) {
     console.log(error)
