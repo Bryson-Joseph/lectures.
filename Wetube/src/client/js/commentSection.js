a9eabe3af6a99311bab1bc3a56b5ecca334361a5
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-03-26 11:00:49 +0300

    rendering-comments-and-realtime-comments

--- Wetube/src/client/js/commentSection.js
+++ Wetube/src/client/js/commentSection.js
@@ -1,7 +1,20 @@
 const videoContainer = document.getElementById('videoContainer')
 const form = document.getElementById('commentForm')
 
-const handleSubmit = (event) => {
+const addComment = (text) => {
+  const videoComments = document.querySelector('.video__comments ul')
+  const newComment = document.createElement('li')
+  newComment.className = 'video__comment'
+  const icon = document.createElement('i')
+  icon.className = 'fas fa-comment'
+  const span = document.createElement('span')
+  span.innerText = `${text}`
+  newComment.appendChild(icon)
+  newComment.appendChild(span)
+  videoComments.prepend(newComment)
+}
+
+const handleSubmit = async (event) => {
   event.preventDefault()
   const textarea = form.querySelector('textarea')
   const text = textarea.value
@@ -10,7 +23,7 @@ const handleSubmit = (event) => {
   if (text === '') {
     return
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
