dfb8c5dc64aa726bbfd0d26e0a7870f0465654d7
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-05-22 15:05:54 +0300

    firebase-project

---
+++ nwitter-reloaded/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_VuOYRSnHi6r5UDv50l4qu6q2wMK7ZE0',
+  authDomain: 'nwitter-reloaded-fe241.firebaseapp.com',
+  projectId: 'nwitter-reloaded-fe241',
+  storageBucket: 'nwitter-reloaded-fe241.appspot.com',
+  messagingSenderId: '627979898787',
+  appId: '1:627979898787:web:68b58e7adb2fd1612c9416',
+}
+
+const app = initializeApp(firebaseConfig)
