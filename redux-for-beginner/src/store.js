94196d6f3e7cb558ba354c3052f3aaf559e33073
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-05-22 09:34:25 +0300

    create-reducer

--- redux-for-beginner/src/store.js
+++ redux-for-beginner/src/store.js
@@ -1,21 +1,22 @@
-import { createStore } from 'redux'
-import { createAction } from '@reduxjs/toolkit'
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit'
 
 const addToDo = createAction('ADD')
 const deleteToDo = createAction('DELETE')
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state]
-    case deleteToDo.type:
+const initialState = []
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() })
+    })
+    .addCase(deleteToDo, (state, action) => {
       return state.filter((toDo) => toDo.id !== action.payload)
-    default:
-      return state
-  }
-}
+    })
+})
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer,
+})
 
 export const actionCreators = {
   addToDo,
