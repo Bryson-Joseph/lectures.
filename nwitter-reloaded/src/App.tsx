a445de40c4f9b7240e1cab4d526242e187150b68
jeff08095 <135233820+jeff08095@users.noreply.github.com>
2024-05-22 16:22:44 +0300

    forms-and-ui

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -4,10 +4,11 @@ import Home from './routes/home'
 import Profile from './routes/profile'
 import Login from './routes/login'
 import CreateAccount from './routes/create-account'
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, styled } from 'styled-components'
 import reset from 'styled-reset'
 import { useEffect, useState } from 'react'
 import LoadingScreen from './components/loading-screen'
+import { auth } from './firebase'
 
 const router = createBrowserRouter([
   {
@@ -45,21 +46,26 @@ const GlobalStyles = createGlobalStyle`
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
   const [isLoading, setLoading] = useState(true)
   const init = async () => {
-    // wait for firebase
+    await auth.authStateReady()
     setLoading(false)
   }
   useEffect(() => {
     init()
   }, [])
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   )
 }
 
--- nwitter-reloaded/src/firebase.ts
+++ nwitter-reloaded/src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA_VuOYRSnHi6r5UDv50l4qu6q2wMK7ZE0',
@@ -10,3 +11,5 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
