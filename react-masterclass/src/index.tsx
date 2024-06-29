9c8db871c61495fd40bc1a10e6aab6e31bcfbac8
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-06-29 08:42:49 +0300

    typing-the-props

--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -1,23 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-const darkTheme = {
-  textColor: 'whitesmoke',
-  backgroundColor: '#111',
-}
-
-const lightTheme = {
-  textColor: '#111',
-  backgroundColor: 'whitesmoke',
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
