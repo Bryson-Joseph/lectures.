d89023bfd3959d17ed02c84452be6a4f8c80d89f
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-07-03 17:12:58 +0300

    themes

--- react-masterclas/src/App.tsx
+++ react-masterclas/src/App.tsx
@@ -1,12 +1,5 @@
-import Circle from './Circle'
-
 function App() {
-  return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
-  )
+  return null
 }
 
 export default App
