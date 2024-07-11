f646a0f77041215b55f00a1dc6cf51b0e57d84f0
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-07-11 17:16:23 +0300

    refs

--- react-masterclas/src/components/Board.tsx
+++ react-masterclas/src/components/Board.tsx
@@ -1,7 +1,7 @@
+import { useRef } from 'react'
 import { Droppable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
 import DragabbleCard from './DraggableCard'
-
 const Wrapper = styled.div`
   width: 300px;
   padding-top: 10px;
@@ -12,19 +12,16 @@ const Wrapper = styled.div`
   flex-direction: column;
   overflow: hidden;
 `
-
 const Title = styled.h2`
   text-align: center;
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
 `
-
 interface IAreaProps {
   isDraggingFromThis: boolean
   isDraggingOver: boolean
 }
-
 const Area = styled.div<IAreaProps>`
   background-color: ${(props) =>
     props.isDraggingOver
@@ -36,16 +33,24 @@ const Area = styled.div<IAreaProps>`
   transition: background-color 0.3s ease-in-out;
   padding: 20px;
 `
-
 interface IBoardProps {
   toDos: string[]
   boardId: string
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onClick = () => {
+    inputRef.current?.focus()
+    setTimeout(() => {
+      inputRef.current?.blur()
+    }, 5000)
+  }
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <input ref={inputRef} placeholder="grab me" />
+      <button onClick={onClick}>click me</button>
       <Droppable droppableId={boardId}>
         {(magic, info) => (
           <Area
