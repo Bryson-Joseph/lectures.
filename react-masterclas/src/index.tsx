08fef9ae163a4b4790868b06d1af296fcfee0f33
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-07-18 12:47:46 +0300

    slider-part-one

--- react-masterclas/src/index.tsx
+++ react-masterclas/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
+import App from './App'
+import { createGlobalStyle } from 'styled-components'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { theme } from './theme'
 
-import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
@@ -61,8 +62,9 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  color:black;
+  color:${(props) => props.theme.white.darker};
   line-height: 1.2;
+  background-color: black;
 }
 a {
   text-decoration:none;
@@ -70,13 +72,17 @@ a {
 }
 `
 
+const client = new QueryClient()
+
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
