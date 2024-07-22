9dcb7cde8f2d685bef8e5a2523ad4ba29e68bfef
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-07-22 14:27:37 +0300

    search-redirect

--- react-masterclas/src/components/Header.tsx
+++ react-masterclas/src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { Link, useMatch } from 'react-router-dom'
+import { Link, useMatch, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { motion, useAnimation, useViewportScroll } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -49,7 +50,7 @@ const Item = styled.li`
   }
 `
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -105,6 +106,10 @@ const navVariants = {
   },
 }
 
+interface IForm {
+  keyword: string
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false)
   const homeMatch = useMatch('/')
@@ -131,6 +136,11 @@ function Header() {
       }
     })
   }, [scrollY, navAnimation])
+  const Navigate = useNavigate()
+  const { register, handleSubmit } = useForm<IForm>()
+  const onValid = (data: IForm) => {
+    Navigate(`/search?keyword=${data.keyword}`)
+  }
   return (
     <Nav variants={navVariants} animate={navAnimation} initial={'top'}>
       <Col>
@@ -156,7 +166,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{ x: searchOpen ? -185 : 0 }}
@@ -170,6 +180,7 @@ function Header() {
               clipRule="evenodd"></path>
           </motion.svg>
           <Input
+            {...register('keyword', { required: true, minLength: 2 })}
             transition={{ type: 'linear' }}
             initial={{ scaleX: 0 }}
             animate={inputAnimation}
--- react-masterclas/src/routes/Search.tsx
+++ react-masterclas/src/routes/Search.tsx
@@ -1,5 +1,10 @@
-function Search(){
-  return null;
+import { useLocation } from 'react-router'
+
+function Search() {
+  const location = useLocation()
+  const keyword = new URLSearchParams(location.search).get('keyword')
+  console.log(keyword)
+  return null
 }
 
-export default Search;
+export default Search
