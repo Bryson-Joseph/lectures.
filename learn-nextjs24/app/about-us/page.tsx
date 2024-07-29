64b122125fc58f479635cbbdf5b2d7bf533e9297
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-07-29 18:45:53 +0300

    not-found-routes

--- learn-nextjs24/app/about-us/page.tsx
+++ learn-nextjs24/app/about-us/page.tsx
@@ -1,3 +1,10 @@
+import Navigation from '../../components/navigation'
+
 export default function About_Us() {
-  return <h1>About-Us</h1>
+  return (
+    <div>
+      <Navigation></Navigation>
+      <h1>About Us</h1>
+    </div>
+  )
 }
