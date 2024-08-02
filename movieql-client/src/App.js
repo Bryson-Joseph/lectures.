dd5c4a662cde87b605e6ef669731970b4153db12
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-02 17:23:31 +0300

    Router

--- movieql-client/src/App.js
+++ movieql-client/src/App.js
@@ -1,3 +1,14 @@
-export default function App(){
-  return <div/>
-};
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Movies from './routes/Movies'
+import Movie from './routes/Movie'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Movies />}/>
+    <Route path="/movies/:id" element={<Movie />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
