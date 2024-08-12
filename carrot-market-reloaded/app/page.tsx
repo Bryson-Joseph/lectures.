e9c27d86080adf963818282ea6158d60ec0f1f5e
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-12 12:28:15 +0300

    Directives

--- carrot-market-reloaded/app/page.tsx
+++ carrot-market-reloaded/app/page.tsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
-        <button className="w-full h-10 bg-black text-white rounded-sexy-name m-tomato">
-          Submit
-        </button>
+      <div className="bg-white shadow-lg p-5  rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
+        <a href="#">Alpha</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   )
