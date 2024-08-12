76774d63b3b6a3fb8c2aff300439fde357f66b94
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-12 17:46:46 +0300

    Home-screen

--- carrot-market-reloaded/app/page.tsx
+++ carrot-market-reloaded/app/page.tsx
@@ -1,10 +1,26 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5  rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
-        <a href="#">Alpha</a>
-        <button className="btn">Submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">Dungeon</h1>
+        <h2 className="text-2xl">Dungeon Market!</h2>
       </div>
-    </main>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/Create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors">
+          Dungeon
+        </Link>
+        <div className="flex gap-2">
+          <span>If you have already have account?</span>
+          <Link href="/Login" className="hover:underline">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
