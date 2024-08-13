8311da1663cef595f94816459d0612de76dce27d
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-13 18:35:17 +0300

    refinement

--- carrot-market-reloaded/app/Create-account/action.ts
+++ carrot-market-reloaded/app/Create-account/action.ts
@@ -2,13 +2,32 @@
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-})
-
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: 'Username must be  a string!',
+        required_error: 'Where is my username???',
+      })
+      .min(3, 'Way too short!!!')
+      .max(10, 'That is too long!')
+      .refine(
+        (username) => !username.includes('potato'),
+        'No potatoes allowed!'
+      ),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .superRefine(({ password, confirm_password }, ctx) => {
+    if (password !== confirm_password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Two passwords should be equal',
+        path: ['confirm_password'],
+      })
+    }
+  })
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get('username'),
@@ -17,6 +36,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get('confirm_password'),
   }
   const result = formSchema.safeParse(data)
-  if (!result.success) return result.error.flatten()
-  redirect('/')
+  if (!result.success) {
+    return result.error.flatten()
+  }
 }
