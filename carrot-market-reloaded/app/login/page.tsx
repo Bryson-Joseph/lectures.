7147176590c9183d1e993b4c9794221f0d2c09ce
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-13 16:03:09 +0300

    useFormState

--- carrot-market-reloaded/app/login/page.tsx
+++ carrot-market-reloaded/app/login/page.tsx
@@ -1,20 +1,21 @@
+'use client'
+
 import FormInput from '../components/form-input'
 import FormButton from '../components/form-btn'
 import SocialLogin from '../components/social-login'
+import { redirect } from 'next/navigation'
+import { useFormState } from 'react-dom'
+import { handleForm } from './action'
 
 export default function LogIn() {
-  async function handleForm(formData: FormData) {
-    'use server'
-    await new Promise((resolve) => setTimeout(resolve, 5000))
-    console.log('logged in!')
-  }
+  const [state, action] = useFormState(handleForm, null)
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">HELLO!</h1>
         <h2 className="text-xl">Log In with Email and Password.</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
@@ -27,7 +28,7 @@ export default function LogIn() {
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          errors={state?.errors ?? []}
         />
         <FormButton text="Log in" />
       </form>
