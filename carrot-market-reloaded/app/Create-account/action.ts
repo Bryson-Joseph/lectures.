c35d6da8988335bb4e84d64080bc0579caa5f5c4
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-13 17:19:47 +0300

    validation errors

--- carrot-market-reloaded/app/Create-account/action.ts
+++ carrot-market-reloaded/app/Create-account/action.ts
@@ -1,7 +1,13 @@
 'use server'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
-const usernameSchema = z.string().min(5).max(10)
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+})
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -10,5 +16,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   }
-  usernameSchema.parse(data.username)
+  const result = formSchema.safeParse(data)
+  if (!result.success) return result.error.flatten()
+  redirect('/')
 }
