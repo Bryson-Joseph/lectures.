0022f8368aa0fe0d4b6dc50d53ff9ccb6180252f
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-15 15:58:50 +0300

    prisma studio

--- carrot-market-reloaded/app/lib/db.ts
+++ carrot-market-reloaded/app/lib/db.ts
@@ -3,14 +3,3 @@ import { PrismaClient } from '@prisma/client'
 const db = new PrismaClient()
 
 export default db
-
-async function testPrisma() {
-  const newUsers = await db.user.create({
-    data: {
-      username: 'Jeff',
-    },
-  })
-  console.log(newUsers)
-}
-
-testPrisma()
