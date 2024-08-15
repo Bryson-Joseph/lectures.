b119b37e25d7f84d733e5386b124048f4a2b76b6
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-15 17:10:10 +0300

    Relationships

--- carrot-market-reloaded/app/lib/db.ts
+++ carrot-market-reloaded/app/lib/db.ts
@@ -3,3 +3,11 @@ import { PrismaClient } from '@prisma/client'
 const db = new PrismaClient()
 
 export default db
+
+db.sMSToken
+  .findMany({
+    include: {
+      user: true,
+    },
+  })
+  .then(console.log)
