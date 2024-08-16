d8032534c8c7e6748b8d81eb4158b0f6eb2adcec
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-16 17:17:50 +0300

    iron session

--- carrot-market-reloaded/app/Create-account/action.ts
+++ carrot-market-reloaded/app/Create-account/action.ts
@@ -7,6 +7,9 @@ import {
 import db from '../lib/db'
 import { z } from 'zod'
 import bcrypt from 'bcrypt'
+import { getIronSession } from 'iron-session'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 const checkUsername = (username: string) => !username.includes('potato')
 
@@ -80,6 +83,7 @@ const formSchema = z
   })
 
 export async function createAccount(prevState: any, formData: FormData) {
+  console.log(cookies())
   const data = {
     username: formData.get('username'),
     email: formData.get('email'),
@@ -107,6 +111,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     })
     console.log(user)
     // log the user in
-    // redirect "/home"
+    const cookie = await getIronSession(cookies(), {
+      cookieName: 'delicious-carrot',
+      password: process.env.COOKIE_PASSWORD!,
+    })
+    //@ts-ignore
+    cookie.id = user.id
+    await cookie.save()
+    redirect('/profile')
   }
 }
