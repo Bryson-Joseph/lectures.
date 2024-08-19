eb22f8930c9730f56dbbf66ba6369f1b7658935b
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-19 16:24:02 +0300

    matcher

--- carrot-market-reloaded/middleware.ts
+++ carrot-market-reloaded/middleware.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import getSession from './app/lib/session'
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession()
-  console.log(session)
-  if (request.nextUrl.pathname === '/profile') {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+  console.log('hello')
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico)*'],
 }
