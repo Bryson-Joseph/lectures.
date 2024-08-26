3cbdeaf619fe080e8cc1040d22e46dae1e77354f
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-08-26 16:34:28 +0300

    product-detail

--- carrot-market-reloaded/prisma/migrations/20240823133108_add_user/migration.sql
+++ carrot-market-reloaded/prisma/migrations/20240823133108_add_user/migration.sql
@@ -24,20 +24,6 @@ CREATE TABLE "SMSToken" (
     CONSTRAINT "SMSToken_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "photo" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
@@ -55,6 +41,3 @@ CREATE UNIQUE INDEX "SMSToken_token_key" ON "SMSToken"("token");
 
 -- AddForeignKey
 ALTER TABLE "SMSToken" ADD CONSTRAINT "SMSToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
