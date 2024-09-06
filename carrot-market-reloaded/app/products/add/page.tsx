0fd91810417d13e28910e86f512aaca717d53b12
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-09-06 12:07:13 +0300

    form action

--- carrot-market-reloaded/app/products/add/page.tsx
+++ carrot-market-reloaded/app/products/add/page.tsx
@@ -4,18 +4,36 @@ import Button from '@/components/button'
 import Input from '@/components/input'
 import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useState } from 'react'
+import { uploadProduct } from './action'
 
 export default function AddProduct() {
   const [preview, setPreview] = useState('')
-  const OnImageChange = () => {}
+  const OnImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event
+    if (!files) {
+      return
+    }
+    const file = files[0]
+    const url = URL.createObjectURL(file)
+    setPreview(url)
+  }
   return (
     <div>
-      <form className="p-5 flex flex-col gap-5">
+      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer ">
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-400 text-sm">Please add a photo</div>
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover "
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}>
+          {preview === '' ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">Please add a photo</div>
+            </>
+          ) : null}
         </label>
         <input
           type="file"
