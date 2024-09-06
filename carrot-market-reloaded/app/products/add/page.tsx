c5abe4c18ccfc0f3093317c7f54fd0604c03c9ea
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-09-06 12:49:13 +0300

    product upload

--- carrot-market-reloaded/app/products/add/page.tsx
+++ carrot-market-reloaded/app/products/add/page.tsx
@@ -5,6 +5,7 @@ import Input from '@/components/input'
 import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useState } from 'react'
 import { uploadProduct } from './action'
+import { useFormState } from 'react-dom'
 
 export default function AddProduct() {
   const [preview, setPreview] = useState('')
@@ -19,9 +20,10 @@ export default function AddProduct() {
     const url = URL.createObjectURL(file)
     setPreview(url)
   }
+  const [state, action] = useFormState(uploadProduct, null)
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover "
@@ -31,7 +33,10 @@ export default function AddProduct() {
           {preview === '' ? (
             <>
               <PhotoIcon className="w-20" />
-              <div className="text-neutral-400 text-sm">Please add a photo</div>
+              <div className="text-neutral-400 text-sm">
+                Please add a photo
+                {state?.fieldErrors.photo}
+              </div>
             </>
           ) : null}
         </label>
@@ -43,13 +48,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input type="text" name="title" placeholder="full" required />
-        <Input type="number" name="price" placeholder="price" required />
+        <Input
+          type="text"
+          name="title"
+          placeholder="full"
+          required
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          type="number"
+          name="price"
+          placeholder="price"
+          required
+          errors={state?.fieldErrors.price}
+        />
         <Input
           type="text"
           name="description"
           placeholder="Detailed Description"
           required
+          errors={state?.fieldErrors.description}
         />
         <Button text="Completed writing" />
       </form>
