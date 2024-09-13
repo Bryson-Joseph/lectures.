20d29e7cf3d2ef404a4987bc75e2d1c26251f7bb
Bryson Joseph <135233820+Bryson-Joseph@users.noreply.github.com>
2024-09-13 12:16:14 +0300

    fonts

--- carrot-market-reloaded/app/layout.tsx
+++ carrot-market-reloaded/app/layout.tsx
@@ -1,17 +1,34 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto, Rubik_Scribble } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  style: ['normal'],
+  variable: '--roboto-text',
+})
+
+const rubick = Rubik_Scribble({
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+  variable: '--rubick-text',
+})
+
+const metallica = localFont({
+  src: './metallica.ttf',
+  variable: '--metallica-text',
+})
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Carrot Market',
-    default: 'Carrot Market',
+    template: '%s | Karrot Market',
+    default: 'Karrot Market',
   },
   description: 'Sell and buy all the things!',
 }
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,7 +38,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         suppressHydrationWarning
-        className={`${inter.className} bg-neutral-900 text-white max-w-screen-md mx-auto`}>
+        className={` ${roboto.variable} ${rubick.variable} ${metallica.variable} bg-neutral-900 text-white max-w-screen-sm mx-auto`}>
         {children}
       </body>
     </html>
