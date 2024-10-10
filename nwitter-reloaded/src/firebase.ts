91b79a5e61232b110ff33a33573788171b4ca46b
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-10-10 10:42:51 +0300

    ....

--- nwitter-reloaded/src/firebase.ts
+++ nwitter-reloaded/src/firebase.ts
@@ -19,3 +19,25 @@ export const auth = getAuth(app)
 export const storage = getStorage(app)
 
 export const db = getFirestore(app)
+
+// firebase.js
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+// const firebaseConfig = {
+//   apiKey: 'YOUR_API_KEY',
+//   authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+//   projectId: 'YOUR_PROJECT_ID',
+//   storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+//   appId: 'YOUR_APP_ID',
+// };
+
+// // Initialize Firebase
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
