0ffb1215409260f0b8e7087eb4217b9296a20e9d
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-10-16 16:26:00 +0300

    typeOrm Setup

--- nuber-eats-backend/src/app.module.ts
+++ nuber-eats-backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   imports: [
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5500,
+      username: 'jambo',
+      password: 'jamboteam',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
