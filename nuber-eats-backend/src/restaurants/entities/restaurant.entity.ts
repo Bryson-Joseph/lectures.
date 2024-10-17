1d41695f3b922426e48936fd928071f3d705a15f
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-10-17 11:01:30 +0300

    Our First Entity

--- nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
+++ nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
