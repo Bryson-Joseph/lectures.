02dd2c9e730248251a13912b7001ebb5faa77bf2
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-10-23 17:09:26 +0300

    Mailgun API

--- nuber-eats-backend/src/mail/mail.service.ts
+++ nuber-eats-backend/src/mail/mail.service.ts
@@ -1,33 +1,33 @@
+import got from 'got';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
-
+import * as FormData from 'form-data';
 @Injectable()
 export class MailService {
   constructor(
-    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+    @Inject(CONFIG_OPTIONS) private readonly opftions: MailModuleOptions,
   ) {
     this.sendEmail('testing', 'test');
   }
   private async sendEmail(subject: string, content: string) {
-    const response = await fetch(
+    const form = new FormData();
+    form.append('from', `Excited User<mailgun@${this.options.domain}>`);
+    form.append('to', `jeffreyjefferson47@gmail.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
         method: 'POST',
         headers: {
-          Authentication: `Basic ${Buffer.from(
+          Authorization: `Basic ${Buffer.from(
             `api:${this.options.apiKey}`,
           ).toString('base64')}`,
         },
-        body: JSON.stringify({
-          from: this.options.fromEmail,
-          to: 'jeffreyjefferson47@gmail.com',
-          subject: subject,
-          text: content,
-        }),
+        body: form,
       },
     );
-    const json = await response.json();
-    console.log(json);
+    console.log(response.body);
   }
 }
