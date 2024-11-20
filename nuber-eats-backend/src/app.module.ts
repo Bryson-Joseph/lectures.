5a68add6c40443c4ccf82f88d41aaa1cf69aad4a
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-11-20 18:51:44 +0300

    cookedOrders

--- nuber-eats-backend/src/app.module.ts
+++ nuber-eats-backend/src/app.module.ts
@@ -67,11 +67,10 @@ import { OrderItem } from './orders/order-items.entity';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       context: ({ req, connection }) => {
-        if (req) {
-          return { user: req['user'] };
-        } else {
-          console.log(connection);
-        }
+        const TOKEN_KEY = 'x-jwt';
+        return {
+          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY],
+        };
       },
       driver: ApolloDriver,
     }),
@@ -87,14 +86,9 @@ import { OrderItem } from './orders/order-items.entity';
     UsersModule,
     RestaurantsModule,
     OrdersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .forRoutes({ path: '/graphql', method: RequestMethod.POST });
-  }
-}
+export class AppModule {}
--- nuber-eats-backend/src/orders/orders.service.ts
+++ nuber-eats-backend/src/orders/orders.service.ts
@@ -10,7 +10,11 @@ import { Order, OrderStatus } from './entities/order.entity';
 import { OrderItem } from './order-items.entity';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
-import { NEW_PENDING_ORDER, PUB_SUB } from 'src/common/common.constants';
+import {
+  NEW_COOKED_ORDER,
+  NEW_PENDING_ORDER,
+  PUB_SUB,
+} from 'src/common/common.constants';
 import { PubSub } from 'graphql-subscriptions';
 
 @Injectable()
@@ -236,12 +240,17 @@ export class OrderService {
           error: "You can't do that.",
         };
       }
-      await this.orders.save([
-        {
-          id: orderId,
-          status,
-        },
-      ]);
+      await this.orders.save({
+        id: orderId,
+        status,
+      });
+      if (user.role === UserRole.Owner) {
+        if (status === OrderStatus.cooked) {
+          await this.pubSub.publish(NEW_COOKED_ORDER, {
+            cookedOrders: { ...order, status },
+          });
+        }
+      }
       return {
         ok: true,
       };
