d58062504a1f26040ed5fda89342a78d36aa389c
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-11-20 12:55:02 +0300

    PUB_SUB

--- nuber-eats-backend/src/common/common.module.ts
+++ nuber-eats-backend/src/common/common.module.ts
@@ -1,3 +1,17 @@
-import { Module } from '@nestjs/common';
-@Module({})
+import { Global, Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { PUB_SUB } from './common.constants';
+
+const pubsub = new PubSub();
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: PUB_SUB,
+      useValue: pubsub,
+    },
+  ],
+  exports: [PUB_SUB],
+})
 export class CommonModule {}
