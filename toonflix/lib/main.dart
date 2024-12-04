30f2d2e09c01717f82f6ad824e1bb66592a12185
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-12-04 11:31:34 +0300

    AppBar

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -11,19 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          surface: const Color(0xFFE7626C),
-          // Allows the homescreen widget to use the theme's surface color for the background as intended.
-        ),
-      ),
-      home: const HomeScreen(),
+      home: Container(),
     );
   }
 }
