9452050bd386769b2dc65cf6df6edff792861579
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-12-16 17:07:28 +0300

    FormButton

--- tiktok_clone/lib/features/authentication/email_screen.dart
+++ tiktok_clone/lib/features/authentication/email_screen.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
+class EmailScreen extends StatefulWidget {
+  const EmailScreen({super.key});
+  @override
+  State<EmailScreen> createState() => _EmailScreenState();
+}
+
+class _EmailScreenState extends State<EmailScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  String _username = "";
+  @override
+  void initState() {
+    super.initState();
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text(
+              "What is your email?",
+              style: TextStyle(
+                  fontSize: Sizes.size24, fontWeight: FontWeight.w700),
+            ),
+            Gaps.v16,
+            TextField()
+          ],
+        ),
+      ),
+    );
+  }
+}
