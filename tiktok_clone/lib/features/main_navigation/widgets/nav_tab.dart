1c306a47031bf9b29a6291fb2ee5b704f18eb8c1
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-12-28 11:25:59 +0300

    stateful Navigation part one

--- tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
+++ tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
@@ -9,10 +9,12 @@ class NavTab extends StatelessWidget {
     required this.isSelected,
     required this.icon,
     required this.onTap,
+    required this.selectedIcon,
   });
   final String text;
   final bool isSelected;
   final IconData icon;
+  final IconData selectedIcon;
   final Function onTap;
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,7 @@ class NavTab extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 FaIcon(
-                  icon,
+                  isSelected ? selectedIcon : icon,
                   color: Colors.white,
                 ),
                 Gaps.v5,
