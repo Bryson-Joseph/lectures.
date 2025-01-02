b0da01ca26b70132b04c89120474dda123bc5703
Bryson Joseph <jeffreyjefferson47@gmail.com>
2025-01-02 17:06:42 +0300

    Video Player

--- tiktok_clone/lib/features/videos/video_timeline_screen.dart
+++ tiktok_clone/lib/features/videos/video_timeline_screen.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
+
   @override
   State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
-  final int _itemCount = 4;
+  int _itemCount = 4;
 
   final PageController _pageController = PageController();
 
-  List<Color> colors = [
-    Colors.blue,
-    Colors.red,
-    Colors.yellow,
-    Colors.teal,
-  ];
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
+
   void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
     if (page == _itemCount - 1) {
-      _pageController.animateToPage(0,
-          duration: const Duration(milliseconds: 100), curve: Curves.linear);
       _itemCount = _itemCount + 4;
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.teal,
-      ]);
       setState(() {});
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,15 +48,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => Container(
-        color: colors[index],
-        child: Center(
-          child: Text(
-            "Screen $index",
-            style: const TextStyle(fontSize: 68),
-          ),
-        ),
-      ),
+      itemBuilder: (context, index) =>
+          VideoPost(onVideoFinished: _onVideoFinished),
     );
   }
 }
