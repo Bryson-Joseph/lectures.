d0c4f508136805fc7a9e0605f0d3b644dec1e8ef
Bryson Joseph <jeffreyjefferson47@gmail.com>
2025-01-15 16:14:37 +0300

    CustomScrollView

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -10,6 +10,27 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return const CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: [
+              StretchMode.zoomBackground,
+              StretchMode.blurBackground,
+              StretchMode.fadeTitle,
+            ],
+            background: Image(
+              image: NetworkImage(
+                  "https://www.vmcdn.ca/f/files/via/images/arts/soft-girl.jpg;w=960"),
+              fit: BoxFit.cover,
+            ),
+            title: Text("Bilkahyr❤️"),
+          ),
+        ),
+      ],
+    );
   }
 }
