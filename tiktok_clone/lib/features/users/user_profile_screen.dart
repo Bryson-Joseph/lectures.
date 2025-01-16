e93bee6bcd99c1c4da1e559fcced0e0e626e7918
Bryson Joseph <jeffreyjefferson47@gmail.com>
2025-01-16 14:44:51 +0300

    VerticalDivider

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -11,59 +13,122 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          pinned: true,
-          stretch: true,
-          backgroundColor: Colors.teal,
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-              StretchMode.fadeTitle,
+    return CustomScrollView(slivers: [
+      SliverAppBar(
+        title: const Text('Bilkahyr❤️'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.gear, size: 20),
+          ),
+        ],
+      ),
+      SliverToBoxAdapter(
+          child: Column(
+        children: [
+          const CircleAvatar(
+            radius: 45,
+            foregroundColor: Colors.teal,
+            foregroundImage: NetworkImage(
+                "https://www.vmcdn.ca/f/files/via/images/arts/soft-girl-thumb.jpg;w=1080;h=885;mode=crop"),
+            child: Text('Bilkahyr❤️'),
+          ),
+          Gaps.v20,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '@Bilkahyr',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                ),
+              ),
+              Gaps.h5,
+              FaIcon(
+                FontAwesomeIcons.solidCircleCheck,
+                size: Sizes.size16,
+                color: Colors.blue.shade500,
+              )
             ],
-            background: Image.network(
-              "https://www.vmcdn.ca/f/files/via/images/arts/soft-girl.jpg;w=960",
-              fit: BoxFit.cover,
+          ),
+          Gaps.v24,
+          SizedBox(
+            height: Sizes.size48,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      "97",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                    Gaps.v5,
+                    Text(
+                      "Following",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    )
+                  ],
+                ),
+                const VerticalDivider(
+                  width: Sizes.size32,
+                  thickness: Sizes.size1,
+                  indent: Sizes.size10,
+                  endIndent: Sizes.size10,
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      "18.2M",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                    Gaps.v5,
+                    Text(
+                      "Followers",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    )
+                  ],
+                ),
+                const VerticalDivider(
+                  width: Sizes.size32,
+                  thickness: Sizes.size1,
+                  indent: Sizes.size10,
+                  endIndent: Sizes.size10,
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      "20.2M",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                    Gaps.v5,
+                    Text(
+                      "Likes",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-            title: const Text("Bikhayr❤️"),
-          ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
-              ),
-            ),
-          ),
-          itemExtent: 100,
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.blue[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
-              ),
-            ),
-          ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        )
-      ],
-    );
+          )
+        ],
+      )),
+    ]);
   }
 }
