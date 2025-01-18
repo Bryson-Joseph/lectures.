1fc38b3aea697ba591071ba44b14900380e658ae
Bryson Joseph <jeffreyjefferson47@gmail.com>
2025-01-18 12:16:12 +0300

    AboutListTile

--- tiktok_clone/lib/features/settings/ settings_screen.dart
+++ tiktok_clone/lib/features/settings/ settings_screen.dart
@@ -6,29 +6,12 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Settings"),
-        ),
-        body: ListWheelScrollView(
-            diameterRatio: 1.5,
-            offAxisFraction: 1.5,
-            itemExtent: 200,
-            children: [
-              for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-                FractionallySizedBox(
-                  widthFactor: 1,
-                  child: Container(
-                    color: Colors.teal,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "Pick me",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 39,
-                      ),
-                    ),
-                  ),
-                ),
-            ]));
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: ListView(
+        children: const [AboutListTile()],
+      ),
+    );
   }
 }
